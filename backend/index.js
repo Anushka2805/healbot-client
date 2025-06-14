16a93d88909dc96812a8d18f5fbd1b312a148f56
Anushka2805 <anushkaggarwal2005gmail.com>
2025-06-14 22:22:35 +0530

    Fix: CORS updated for Vercel frontend

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ const reportRoutes = require('./routes/reportRoutes'); // ✅ now working
 const app = express();
 
 app.use(cors({
-  origin: ['http://localhost:3000', 'https://healbot-frontend.vercel.app'], // 👈 Add your Vercel URL here
+  origin: ['http://localhost:3000', 'https://healbot-client-rouge.vercel.app/'], // 👈 Add your Vercel URL here
   credentials: true
 }));
 app.use(express.json());
